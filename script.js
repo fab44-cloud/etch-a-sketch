5e9890e7c587a50359d03d01a1388624ed55c434
Ford Berry <fordaberry@gmail.com>
2025-05-26 15:54:33 -0600

    feat: create new div called etch and append it to the container div.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get a reference to the container div in the html.
+let container = document.querySelector("#container");
+// Create a new div and store it in the variable "etch".
+let etch = document.createElement("div");
+etch.classList.add("etch");
+// Append the div to container.
+container.appendChild(etch);
